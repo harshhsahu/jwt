387a35f1c99a2a80360ffdd98e819ddb4fedf3a0
abhinay <abhinay@abhinays-MacBook-Air.local>
2025-03-16 20:00:35 +0530

    fix bug

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,27 @@
+// import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+        port: '',
+        pathname: '**',
+      },
+    ],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  }
   /* config options here */
 };
+
+
 
 export default nextConfig;
